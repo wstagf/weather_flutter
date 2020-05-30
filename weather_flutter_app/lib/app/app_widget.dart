15d2e28c795704acc46a35e2da81639d3b7280af
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-05-29 23:59:48 -0300

    inicializando navigator.key

--- weather_flutter_app/lib/app/app_widget.dart
+++ weather_flutter_app/lib/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:get/get.dart';
 
 class AppWidget extends StatelessWidget {
   @override
@@ -9,6 +10,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      navigatorKey: Get.key,
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
     );
