c6ed4a127a150095804f7f820852b9294a5727bd
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-05-30 00:43:08 -0300

    convertido splash em StatefulWidget para fazer o request a api

--- weather_flutter_app/lib/app/splash/splash_widget.dart
+++ weather_flutter_app/lib/app/splash/splash_widget.dart
@@ -2,7 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:weather_flutter_app/app/utils/size_utils.dart';
 import 'package:weather_flutter_app/app/utils/theme_utils.dart';
 
-class SplashWidget extends StatelessWidget {
+class SplashWidget extends StatefulWidget {
+  @override
+  _SplashWidgetState createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends State<SplashWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeUtils.init(context);
@@ -11,12 +22,13 @@ class SplashWidget extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
+                stops: [0.5, 0.8],
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 colors: [
-              ThemeUtils.getColor('color2'),
-              ThemeUtils.getColor('color3'),
-            ])),
+                  ThemeUtils.getColor('color2'),
+                  ThemeUtils.getColor('color3'),
+                ])),
         child: Center(
           child: CircularProgressIndicator(
             valueColor: AlwaysStoppedAnimation<Color>(
