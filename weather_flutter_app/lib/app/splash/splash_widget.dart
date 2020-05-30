d863787c9f2926cd08d2eddefdadd36331925389
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-05-30 00:00:13 -0300

    criado tela de splash

--- weather_flutter_app/lib/app/splash/splash_widget.dart
+++ weather_flutter_app/lib/app/splash/splash_widget.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:weather_flutter_app/app/utils/size_utils.dart';
+import 'package:weather_flutter_app/app/utils/theme_utils.dart';
 
 class SplashWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("textFinder"),
+    SizeUtils.init(context);
+    return Scaffold(
+      backgroundColor: ThemeUtils.getColor('color1'),
+      body: CircularProgressIndicator(),
     );
   }
 }
