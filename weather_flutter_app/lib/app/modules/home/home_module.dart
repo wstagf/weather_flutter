7f2a935aaeaa8b1dc81e1f9f377da7920c4df863
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-05-29 23:33:16 -0300

    Alterado pagina inicial para o splah

--- weather_flutter_app/lib/app/modules/home/home_module.dart
+++ weather_flutter_app/lib/app/modules/home/home_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:weather_flutter_app/app/modules/home/home_page.dart';
+import 'package:weather_flutter_app/app/splash/splash_widget.dart';
 
 class HomeModule extends ChildModule {
   @override
@@ -7,7 +8,7 @@ class HomeModule extends ChildModule {
 
   @override
   List<Router> get routers => [
-        Router('/', child: (_, args) => HomePage()),
+        Router('/', child: (_, args) => SplashWidget()),
       ];
 
   static Inject get to => Inject<HomeModule>.of();
