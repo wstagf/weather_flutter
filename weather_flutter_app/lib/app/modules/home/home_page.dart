beb6edc7ecb9f3e6dc537d6933409c5f1cb3d42d
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-05-31 14:18:30 -0300

    Colocado modelo das variavies que serao exibidas

--- weather_flutter_app/lib/app/modules/home/home_page.dart
+++ weather_flutter_app/lib/app/modules/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_flutter_app/app/utils/theme_utils.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -9,15 +12,95 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  FlareController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
-      ),
+      backgroundColor: ThemeUtils.getColor('color1'),
+      body: Stack(children: [
+        Center(
+          child: FlareActor("assets/animations/City-cloud.flr",
+              controller: controller,
+              alignment: Alignment.center,
+              fit: BoxFit.contain,
+              isPaused: false,
+              animation: "stop"),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              stops: [0.0, 0.9],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                ThemeUtils.getColor('color1').withAlpha(200),
+                ThemeUtils.getColor('color4').withAlpha(120),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                "Goiânia",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 36),
+              ),
+              Text(
+                "condition",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 28),
+              ),
+              Text(
+                "temperature °C",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                "sensation °C",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                "wind_direction",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                "wind_velocity",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                "humidity",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                "wind_direction",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                "wind_velocity",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
