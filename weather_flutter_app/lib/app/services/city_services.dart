85a5f99d7d496eb41e7f3a3bd427d898e3ddfb77
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-05-31 23:06:52 -0300

    criado configuracao no env para o api path

--- weather_flutter_app/lib/app/services/city_services.dart
+++ weather_flutter_app/lib/app/services/city_services.dart
@@ -1,12 +1,13 @@
 import 'package:weather_flutter_app/app/models/city.dart';
 import 'package:weather_flutter_app/app/utils/custom_dio.dart';
+import 'package:weather_flutter_app/environments/env.dart';
 
 class CityService {
   Future<List<City>> getCities() async {
     List<City> list = [];
     try {
       final dio = CustomDio.whithAutentication().instance;
-      List<dynamic> data = await dio.get('api/v1/locale/city').then((res) {
+      List<dynamic> data = await dio.get('$apiPath/locale/city').then((res) {
         return res.data.toList();
       });
 
